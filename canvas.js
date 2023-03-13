bcd7a6fa577d7ad902928a03b5fad26e438063fb
soyeolesafm <soyelesafm@hotmail.com>
2023-03-13 18:33:53 -0500

    now the glitch is fixed and add random colors

--- canvas.js
+++ canvas.js
@@ -4,16 +4,19 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 let c = canvas.getContext('2d')
 
-function Circle(x, y, dx, dy, rad){
+function Circle(x, y, dx, dy, rad, color){
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.rad = rad;
+    this.color = color;
     this.draw = function (c){
+        c.fillStyle = this.color
         c.beginPath()
         c.arc(this.x,this.y,this.rad,0,Math.PI * 2, false)
         c.stroke()
+        c.fill()
     }
     this.movement = function() {
         
@@ -32,15 +35,15 @@ function Circle(x, y, dx, dy, rad){
 }
 const circles = []
 
-for (let i = 0; i < 100; i++){
+for (let i = 0; i < 200; i++){
     const rad = Math.floor(Math.random() * 50) ;
     const x = Math.floor(Math.random() * (window.innerWidth - rad * 2) + rad);
     const y = Math.floor(Math.random() * (window.innerHeight - rad *2) + rad );
     const dx = Math.floor(Math.random() * 5) + 1;
     const dy = Math.floor(Math.random() * 5) + 1;
-    
+    const color = `rgb(${Math.random() *255}, ${Math.random() *255}, ${Math.random() *255})`
   
-    circles.push(new Circle(x, y, dx, dy, rad))
+    circles.push(new Circle(x, y, dx, dy, rad, color))
   
 
 
@@ -63,3 +66,4 @@ animate()
 // multiples circles
 
 console.log(circles);
+console.log(c);
